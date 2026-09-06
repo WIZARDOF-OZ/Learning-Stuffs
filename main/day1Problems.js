b799360e33e8e179f9526a57d68ce67c3d8a61a6
WIZARD OF OZ <nurajabegum81@gmail.com>
2026-09-06 23:41:58 +0530

    feat: add FizzBuzz problem and basic function examples in day1Problems.js and day2functions.js

--- main/day1Problems.js
+++ main/day1Problems.js
@@ -57,14 +57,15 @@ for (i = 1; i <= 100; i++) {
 
 
 // problem 5
-for (i = 1; i <= 50; i++) {
-    if (i % 3 === 0) {
-        console.log("Fizz")
-    }
-    else if (i % 5 === 0) {
-        console.log("Buzz")
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz")
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
     }
 }
 
---
+++ main/day2functions.js
@@ -0,0 +1,28 @@
+// problem 1
+
+function sayName(name) {
+    console.log(`Hello, ${name}!!`)
+}
+sayName("Dev")
+
+// problem 2
+const number = (a, b) => {
+    return console.log(a * b)
+}
+number(10, 20)
+
+// problem 3
+const taxRate = 0.08; // 8% tax rate
+
+function calculateTotal(price, taxRate) {
+
+    return price + (price * taxRate);
+}
+calculateTotal(100, taxRate); // returns 108
+calculateTotal(100, 0.15); // returns 115
+
+
+function calculator(num1, num2, operator) {
+if 
+
+}
